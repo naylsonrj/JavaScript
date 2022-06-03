c4451884097c0cca199578a533c06dd64baa31a7
Naylson Costa <naylsonrj@gmail.com>
2022-06-03 20:03:07 -0300

    js

--- JS Funcional e Reativo/condicionais/script.js
+++ JS Funcional e Reativo/condicionais/script.js
@@ -7,3 +7,12 @@
 // < menor
 // >= maior ou igual
 // <= menor ou igual
+var naylson = 25;
+var amiely = 32;
+if (naylson > amiely) {
+  console.log("é maior");
+} else if (naylson < amiely) {
+  console.log("é menor");
+} else {
+  console.log("Naylson e Amiely são iguais");
+}
