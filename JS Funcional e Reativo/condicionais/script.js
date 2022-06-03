cf2cbf4a9e2bf1ab908e25626c5a2b9f6d99e4b2
Naylson Costa <naylsonrj@gmail.com>
2022-06-03 18:55:15 -0300

    add examples

---
+++ JS Funcional e Reativo/condicionais/script.js
@@ -0,0 +1,9 @@
+// operadores de comparação
+// == igual
+// === identico
+// != diferente
+// !== diferente
+// > maior
+// < menor
+// >= maior ou igual
+// <= menor ou igual
---
+++ JS Funcional e Reativo/variaveis/script.js
@@ -0,0 +1,19 @@
+// variaveis
+
+var nome = "Naylson";
+var idade = 23;
+var peso = 80.5;
+var humano = true;
+
+console.log(nome, idade, peso, humano);
+
+var comida;
+comida = "Pizza";
+console.log(comida);
+
+/* variaveis pode iniciar com $, _ ou letras
+ variaveis não podem começar com números
+ variaveis não podem ter espaços
+ variaveis não podem ter caracteres especiais
+ variaveis não podem ter acentos
+ existem palavas reservadas que não podem ser utilizadas. */
