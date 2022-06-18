42cecff708c449fa33448df6eb1c095293b2f750
Naylson Costa <naylsonrj@gmail.com>
2022-06-18 15:15:35 -0300

    Conceito de FUNÇÃO DE PURA e IMPURA

---
+++ funcional-principio/funcao_pura_1.js
@@ -0,0 +1,14 @@
+/*
+CONCEITO DE FUNÇÃO DE PURA:
+- Uma função pura é uma função em que o VALOR de retorno é determinado APENAS 
+por seus valores de ENTRADA, sem efeitos colaterais observáveis
+
+*/
+
+// Função IMPURA -> Porque a função depende de PI que é um valor externo
+const PI = 3.14;
+
+function areaCircuferencia(raio) {
+  return raio * raio * PI;
+}
+console.log(areaCircuferencia(10));
