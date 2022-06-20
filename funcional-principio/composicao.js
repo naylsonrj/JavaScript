241d1ffcdcac96894c467946756ca87a8f2e1931
Naylson Costa <naylsonrj@gmail.com>
2022-06-19 21:06:25 -0300

    ajustes de composição

--- funcional-principio/composicao.js
+++ funcional-principio/composicao.js
@@ -10,11 +10,18 @@ function textoAlto(texto) {
   return texto.toUpperCase();
 }
 function textoAtencao(texto) {
-  return `${texto} !!!!!`;
+  return `${texto} !!!`;
 }
 function textoEspacado(texto) {
   return texto.split("").join(" ");
 }
 
-const resultado = composicao(textoAlto, textoAtencao, textoEspacado)("naylson");
-console.log(resultado);
+// const resultado = composicao(textoAlto, textoAtencao, textoEspacado)("naylson");
+// console.log(resultado);
+
+const padraoGeral = composicao(textoAlto, textoAtencao, textoEspacado);
+const resultado1 = padraoGeral("naylson");
+const resultado2 = padraoGeral("naylson 2022");
+
+console.log(resultado1);
+console.log(resultado2);
