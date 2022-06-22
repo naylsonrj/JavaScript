d1ffa94234353580c82338483ef1d7f97c998650
Naylson Costa <naylsonrj@gmail.com>
2022-06-22 20:41:27 -0300

    usando rxjs interval e from

--- rxjs/stream_dados_3.js
+++ rxjs/stream_dados_3.js
@@ -1,1 +1,15 @@
-const rxjs = require("rxjs");
+const { interval, from } = require("rxjs");
+
+const gerarNumeros = interval(500);
+
+const sub1 = gerarNumeros.subscribe((num) => {
+  console.log(Math.pow(2, num)); //
+});
+
+const sub2 = gerarNumeros.subscribe(console.log);
+
+setTimeout(() => sub1.unsubscribe(), 8000);
+setTimeout(() => sub2.unsubscribe(), 6000);
+
+// usando from
+from(["Naylson", "developer", "usando from"]).subscribe(console.log);
