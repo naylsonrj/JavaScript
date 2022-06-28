559e23b2764b6ef6818cae635bf5426118e848f4
Naylson Costa <naylsonrj@gmail.com>
2022-06-27 22:21:19 -0300

    ajustando desafio 6 rxjs

--- rxjs/operadores_6.js
+++ rxjs/operadores_6.js
@@ -1,60 +1,54 @@
 const { from, Observable } = require("rxjs");
 
-// desafio criando uma função que facilita a utlização de novos operadores,
-// usando exemplo de operadores_4.js
-
-function createPipeableOperator(nextGenerator) {
+function createPipeableOperator(operatorFn) {
   return function (source) {
     return Observable.create((subscriber) => {
+      const sub = operatorFn(subscriber);
       source.subscribe({
-        next: nextGenerator(subscriber),
+        next: sub.next,
+        error: sub.error || ((e) => subscriber.error(e)),
+        complete: sub.complete || ((e) => subscriber.complete(e)),
       });
     });
   };
 }
 
 function primeiro() {
-  return createPipeableOperator((subscriber) => (v) => {
-    subscriber.next(v);
-    subscriber.complete();
-  });
+  return createPipeableOperator((subscriber) => ({
+    next(v) {
+      subscriber.next(v);
+      subscriber.complete();
+    },
+  }));
 }
 
 function nenhum() {
-  return function (source) {
-    return Observable.create((subscriber) => {
-      source.subscribe({
-        next(v) {
-          subscriber.complete();
-        },
-      });
-    });
-  };
+  return createPipeableOperator((subscriber) => ({
+    next(v) {
+      subscriber.complete();
+    },
+  }));
 }
 
 function ultimo() {
-  return function (source) {
-    return Observable.create((subscriber) => {
-      let ultimo;
-      source.subscribe({
-        next(v) {
-          ultimo = v;
-        },
-        complete() {
-          if (ultimo !== undefined) {
-            subscriber.next(ultimo);
-          }
-          subscriber.complete();
-        },
-      });
-    });
-  };
+  let ultimo;
+  return createPipeableOperator((subscriber) => ({
+    next(v) {
+      ultimo = v;
+    },
+    complete() {
+      if (ultimo !== undefined) {
+        subscriber.next(ultimo);
+      }
+      subscriber.complete();
+    },
+  }));
 }
 
 from([1, 2, 3, 4, 5])
   .pipe(
-    primeiro()
+    // primeiro(),
     // nenhum(),
-    // ultimo()
+    ultimo()
   )
   .subscribe(console.log);
